502d5ddd24fadb28b83e99e28097cf51d65d2ae7
MBURU-25 <njerimburu07@gmail.com>
2022-10-18 05:11:54 -0700

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - function to print abc
+ * Return: Always 0
  */
 void print_alphabet(void)
-{	
-     	 char letter;
+{
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
-	
+	}
 	_putchar('\n');
-}
 
+	return (0);
+}
