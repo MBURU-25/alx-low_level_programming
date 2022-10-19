bbd241cb79c1da9b83d0fdbf4a3c2b4d1e13e7f0
MBURU-25 <njerimburu07@gmail.com>
2022-10-18 22:59:04 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,6 +15,7 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
@@ -30,10 +31,5 @@ void times_table(void)
 			}
 		}
 		_putchar('\n');
-
 	}
-}	
-	
-	
-
-			
+}
