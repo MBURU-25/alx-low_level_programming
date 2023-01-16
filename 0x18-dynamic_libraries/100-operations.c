7d3072ce4ea570a69d80f6da10ad91a295a6b942
MBURU-25 <njerimburu07@gmail.com>
2023-01-16 03:33:58 -0800

    c from python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * add -sum of two integers
+ * return sum
+**/
+
+int add(int a , int b)
+{
+	return(a + b);
+}
+
+/**
+ * sub -subtract two numbers
+ * return subtract
+**/
+
+int sub(int a , int b)
+{
+	return(a - b);
+}
+
+/**
+ * mul -multiply two numbers
+ * return mul
+**/ 
+
+int mul(int a , int b)
+{
+	return(a * b);
+}
+
+/**
+ * div -divide two numbers
+ * return div
+**/
+
+int div(int a , int b)
+{
+	return(a / b);
+}
+
+/**
+ * mod -modulus of two numbers
+ * return mod
+**/
+
+int mod(int a , int b)
+{
+	return(a % b);
+}	
